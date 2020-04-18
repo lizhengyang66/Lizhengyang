e0d165e234657f71e77a22557bc54220ef140808
Liu-Hai-Wen <2543299395@qq.com>
2020-04-18 22:32:09 +0800

    dadad

--- spring-boot-hello/src/main/java/com/xz/App.java
+++ spring-boot-hello/src/main/java/com/xz/App.java
@@ -8,10 +8,10 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 		//李子阳
+		System.out.println("卢本伟牛逼！！！");
 	}
 	//邹衡滔
 	//刘海文
 	//我为了一切毁灭一切
-    //那是真滴牛逼
-	//lbwnb
+    
 }
